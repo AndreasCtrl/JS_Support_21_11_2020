f71bfbce8ea8ec5b585e554f4c502e79a29642a6
David Oster <paspa@hotmail.com>
2020-11-21 15:10:14 +0200

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,26 @@
+"use strict"
+let b = 20;
+
+if(typeof firstName !== 'undefined') {
+    let firstName ="some first name";
+    let lastName = "some last name";
+    let email = "some email";       
+}
+
+
+// https://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
